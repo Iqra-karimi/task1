369c7115883e535c00eaa734cc53ecd772d60f8f
Iqra-karimi <iqrakarimi56@gmail.com>
2024-10-25 17:23:09 -0700

    fatima

---
+++ task02.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Include the iostream library for input/output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Output Hello, World!
+    return 0;  // Indicate that the program ended successfully
+}
